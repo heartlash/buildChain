8dea787b1005dcff988592697ff2086740414203
root <root@localhost.localdomain>
2019-07-27 03:39:19 +0530

    get for transactions endpoint

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const Blockchain = require('../blockchain');
 const bodyParser = require('body-parser');
 const P2pServer = require('./p2pServer');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 //bc.addBlock("heyo");
@@ -24,6 +28,10 @@ app.post('/mine', (req, res) => {
     console.log(`New block added: ${block.toString()}`);
     p2pServer.syncChain();
     res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
 })
 app.listen(HTTP_PORT, ()=>{
     console.log(`I am listening on port ${HTTP_PORT} or am I`);
